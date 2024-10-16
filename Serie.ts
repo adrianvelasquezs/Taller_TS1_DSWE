9ef97a6feba918caf8421d0d52eaabcac0215060
Adrian Velasquez <a.velasquezs@uniandes.edu.co>
2024-10-16 09:04:08 -0500

    modifications to Serie.ts

--- Serie.ts
+++ Serie.ts
@@ -1,13 +1,23 @@
 export class Serie {
-    constructor(
-        public id: number,
-        public name: string,
-        public channel: string,
-        public seasons: number,
-        public description: string,
-        public link: string,
-        public imageUrl: string
-    ) {}
+    private id: number;
+    private name: string;
+    private channel: string;
+    private seasons: number;
+    private description: string;
+    private link: string;
+    private imageUrl: string;
+
+    constructor( id: number, name: string, channel: string, seasons: number, description: string,
+                 link: string, imageUrl: string) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.description = description;
+        this.link = link;
+        this.imageUrl = imageUrl;
+    }
+
     getName(): string {
         return this.name;
     }
