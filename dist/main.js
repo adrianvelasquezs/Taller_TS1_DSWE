cae2166c00c83fd47ff0844fdb0178e7297a46f2
Adrian Velasquez <a.velasquezs@uniandes.edu.co>
2024-10-19 15:59:15 -0500

    final commit

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,13 @@
 import { series } from './data.js';
-var tableBody = document.getElementById("seriesTableBody");
-renderTable(series);
+var tableBody = document.getElementById("seriesTableBody"); // table main body
+var avgSeasons = document.getElementById("avgSeasons"); // average seasons body
+renderTable(series); // render the table with the series
+// render the average number of seasons
+avgSeasons.innerHTML = "Average number of seasons: " + calculateAverageSeasons(series).toString();
+/**
+ * Render the table with the series
+ * @param series
+ */
 function renderTable(series) {
     tableBody.innerHTML = '';
     series.forEach(function (serie) {
@@ -19,4 +26,15 @@ function renderTable(series) {
         row.appendChild(seasonsCell);
         tableBody.appendChild(row);
     });
+}
+/**
+ * Calculate the average number of seasons of the series
+ * @param series
+ */
+function calculateAverageSeasons(series) {
+    var sum = 0;
+    series.forEach(function (serie) {
+        sum += serie.seasons;
+    });
+    return sum / series.length;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { Serie } from './serie.js';
 import { series } from './data.js';
 
-const tableBody: HTMLElement = document.getElementById("seriesTableBody")!;
+const tableBody: HTMLElement = document.getElementById("seriesTableBody")!; // table main body
+const avgSeasons: HTMLElement = document.getElementById("avgSeasons")!; // average seasons body
 
-renderTable(series);
+renderTable(series); // render the table with the series
 
+// render the average number of seasons
+avgSeasons.innerHTML = "Average number of seasons: " + calculateAverageSeasons(series).toString();
+
+/**
+ * Render the table with the series
+ * @param series
+ */
 function renderTable(  series: Serie[] ): void {
     tableBody.innerHTML = '';
     series.forEach(serie => {
@@ -28,5 +36,17 @@ function renderTable(  series: Serie[] ): void {
 
         tableBody.appendChild(row);
     });
+}
+
+/**
+ * Calculate the average number of seasons of the series
+ * @param series
+ */
+function calculateAverageSeasons( series: Serie[] ): number {
+    let sum = 0;
+    series.forEach(serie => {
+        sum += serie.seasons;
+    });
+    return sum / series.length;
 }
 
